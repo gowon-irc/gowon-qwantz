d4113516eb05ae4d0b3384487ac29d5a32df7aa2
shanedabes <shane@isda.best>
2021-10-21 23:36:39 +0100

    add qwantz gowon module

---
+++ qwantz_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func testComic(t *testing.T) {
+	c := comic{
+		url:  "test.com",
+		desc: "comic desc",
+	}
+
+	expected := "comic desc - test.com"
+
+	assert.Equal(t, c.String(), expected)
+}
